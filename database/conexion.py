7f7d54db8ed3e3b1f7c10b05f3c208ca233868e1
Carloscu34 <ccum2@miumg.edu.gt>
2026-05-24 21:44:24 -0600

    conexion mysql hostim

--- database/conexion.py
+++ database/conexion.py
@@ -3,10 +3,11 @@ import mysql.connector
 def conectar():
 
     conexion = mysql.connector.connect(
-        host="localhost",
-        user="flaskuser",
-        password="1234",
-        database="bd_biometrico"
+        host="biometrico-mysql.hpr-0f2570c4.svc",
+	port=3306,
+        user="hpr-0f2570c4-biometrico",
+        password="QrIfHB6yDdvN2XaB",
+        database="hpr-0f2570c4-biometrico"
     )
 
     return conexion
